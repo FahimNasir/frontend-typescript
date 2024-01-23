07e1b6b882d95b8d9536d881ca525f9fa03a9621
Fahim Nasir <fahimnasir1993@gmail.com>
2024-01-23 15:27:51 +0500

    Initial Commit

---
+++ src/components/authentication/Login.jsx
@@ -0,0 +1,14 @@
+const Login = () => {
+  const placeHolderTextAdditional = "Additional";
+  return (
+    <>
+      <label>Email Address</label>
+      <input
+        className=""
+        placeholder={`Place holder value is ${placeHolderTextAdditional}`}
+      />
+    </>
+  );
+};
+
+export default Login;
